9fce084672d07edf7b4cc4d1de2d98a11d3dbce1
murakami-m <senazuki0825@yahoo.co.jp>
2022-02-01 21:02:35 +0900

    入院患者一覧タブの変更

---
+++ app/javascript/current.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+})
